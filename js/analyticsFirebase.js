787c907c7143d1198fba06aba80674b8b18c35f9
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2025-02-01 10:19:24 +0200

    update analytics

--- js/analyticsFirebase.js
+++ js/analyticsFirebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-console.log(analytics);
+// document.getElementById("myButton").addEventListener("click", () => {
+//     logEvent(analytics, 'button_click', {
+//         button_name: 'My Button',
+//         timestamp: new Date().toISOString(),
+//     });
+// });
